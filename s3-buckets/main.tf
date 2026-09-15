63ee34608c9efdf1a5cbb9bed019112806155af2
JudeCodeHub <judechihan727@gmail.com>
2026-09-15 16:59:39 +0530

    Initial Project Setup for DevOps Configuration

---
+++ s3-buckets/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket1" {
+  bucket = var.bucket1_name
+
+  tags = {
+    Name        = var.bucket1_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bucket1_versioning" {
+  bucket = aws_s3_bucket.bucket1.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket" "bucket2" {
+  bucket = var.bucket2_name
+
+  tags = {
+    Name        = var.bucket2_name
+    Environment = var.environment
+  }
+}
+
+resource "aws_s3_bucket_versioning" "bucket2_versioning" {
+  bucket = aws_s3_bucket.bucket2.id
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
---
+++ s3-buckets/variables.tf
@@ -0,0 +1,17 @@
+variable "bucket1_name" {
+  description = "Name of the first S3 bucket for Terraform state"
+  type        = string
+  default     = "ecommerce-microservices-tfstate-bucket-1"
+}
+
+variable "bucket2_name" {
+  description = "Name of the second S3 bucket for Terraform state"
+  type        = string
+  default     = "ecommerce-microservices-tfstate-bucket-2"
+}
+
+variable "environment" {
+  description = "Environment tag for the buckets"
+  type        = string
+  default     = "dev"
+}
